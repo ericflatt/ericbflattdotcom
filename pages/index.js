61a7079dbfe3bdfc533c3d763e693183768092b9
ericflatt <eflatt18@gmail.com>
2022-03-05 20:15:12 -0800

    updates

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 import Head from "next/head";
-import React, { useEffect } from "react";
-import Image from "next/image";
-import funko from "../pages/images/funko-me.png";
-import Script from "next/script";
+import React, { useEffect, useState } from "react";
+// import Image from "next/image";
+// import Script from "next/script";
 import Clock from "react-live-clock";
 
 export default function Home() {
-  useEffect(() => {
-    if (document) {
-    }
-  }, []);
+  // useEffect(() => {
+  // }, []);
 
   return (
     <div className="container">
@@ -20,35 +17,33 @@ export default function Home() {
           name="description"
           content="Eric Flatt is a UX Designer currently working at Funko"
         />
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=[UA-107339411-2]"
-        />
-
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '[UA-107339411-2]', { page_path: window.location.pathname });
-            `,
-          }}
-        />
       </Head>
       {/* main section */}
       <main className="mx-auto mt-16 max-w-container-small md:max-w-container-large md:mt-32">
-        <div className="tooltip">
+        {/* <div className="tooltip">
           <button className="bread">🍞</button>
           <span className="tooltiptext">
             Why the bread? Because I'm allergic
           </span>
-        </div>
-        <h1 className="text-base">Eric Flatt</h1>
-        <h2 className="text-secondary">Product Designer based in Seattle</h2>
+        </div> */}
+        <h1 className="text-base">Hello, my name is Eric.</h1>
+        {/* <h2 className="text-secondary">Product Designer based in Seattle</h2> */}
         <div>
           <p className="bio">
-            I'm currently a UI/UX Designer on the digital experience team at{" "}
+            I'm a product designer based in Seattle. Right now I am working on
+            the digital experience team at {" "}
+            <a
+              target="blank"
+              href="https://funko.com"
+              className="content__item"
+            >
+              <button className="button button--narvi">
+                <span>
+                  <span style={{ fontWeight: "700" }}> Funko</span>
+                </span>
+              </button>
+            </a>.
+            {/* I'm currently a UI/UX Designer on the digital experience team at{" "}
             <a className="text-link" href="https://funko.com" target="blank">
               Funko
             </a>
@@ -58,7 +53,7 @@ export default function Home() {
             <a href="mailto:eflatt18@gmail.com" className="text-link">
               get in touch
             </a>
-            .{" "}
+            .{" "} */}
           </p>
         </div>
         {/* social links */}
